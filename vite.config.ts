853ef0ff50cc56870656b39c69092dd904d20f90
Hundo1018 <f74169@gmail.com>
2022-11-05 01:52:54 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://github.com/Hundo1018/HundoWeb.git",
+  base:"https://github.com/Hundo1018/HundoWeb.git/",
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
